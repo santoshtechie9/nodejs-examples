106c14ced0bd35c21e0c9c9df213492647fdd323
santoshtechie9 <santoshtechie9@gmail.com>
2017-10-27 09:01:14 -0700

    object literals

---
+++ modules/app.js
@@ -0,0 +1,7 @@
+
+
+// require function with make the module.exports available
+var greet = require('./greet.js');
+
+// greet function is defined in the greet.js module
+greet();
---
+++ modules/greet.js
@@ -0,0 +1,9 @@
+
+
+// function extression
+var greet = function() {
+    console.log("Hello!");    
+}
+
+// module.exports will make the greet function available for other modules
+module.exports = greet;
